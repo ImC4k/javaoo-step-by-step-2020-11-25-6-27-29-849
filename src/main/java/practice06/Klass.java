661996d973eee3882a487e987f468de2c767463d
Calvin Chu <calvin.chu@oocl.com>
2020-11-25 15:05:39 +0800

    [Calvin] feat: pracice 6

--- src/main/java/practice06/Klass.java
+++ src/main/java/practice06/Klass.java
@@ -1,4 +1,21 @@
 package practice06;
 
 public class Klass {
+    private Integer number;
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return String.format("Class %d", number);
+    }
 }
--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,39 @@
 package practice06;
 
-public class Teacher {
+import practice05.Person;
+
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return klass == null?
+                super.introduce() + " I am a Teacher. I teach No Class."
+                :
+                super.introduce() + String.format(" I am a Teacher. I teach Class %d.", klass.getNumber());
+    }
+
+    public String introduceWith(Student student) {
+        if (klass == null || student.getKlass().getNumber() != klass.getNumber())
+            return super.introduce() + String.format(" I am a Teacher. I don't teach %s.", student.getName());
+        else
+            return super.introduce() + String.format(" I am a Teacher. I teach %s.", student.getName());
+    }
 }
