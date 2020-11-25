37d27b0389212db2ceaad705b02a7882cec62774
Calvin Chu <calvin.chu@oocl.com>
2020-11-25 17:07:39 +0800

    [Calvin] feat: pracice 10

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,59 @@
 package practice10;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    private Integer number;
+    private Student leader;
+    private List<Student> students = new ArrayList<>();
+
+    private Teacher teacher = null;
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void assignLeader(Student newLeader) {
+        if (!students.contains(newLeader)) {
+            System.out.print("It is not one of us.\n");
+        }
+        else {
+            this.leader = newLeader;
+            if (teacher != null) {
+                teacher.handleClassLeaderChange(this, newLeader);
+            }
+        }
+    }
+
+    public String getDisplayName() {
+        return String.format("Class %d", number);
+    }
+
+    public void appendMember(Student newStudent) {
+        students.add(newStudent);
+        if (teacher != null) {
+            teacher.handleNewStudentJoiningClass(this, newStudent);
+        }
+    }
+
+    public boolean isIn(Student student) {
+        return student.getKlass().getNumber() == this.number;
+    }
+
+    public void assignTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,59 @@
 package practice10;
 
-public class Teacher {
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Teacher extends Person {
+    private List<Klass> klasses;
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, List<Klass> klasses) {
+        super(id, name, age);
+        this.klasses = klasses;
+        klasses.forEach(klass -> klass.assignTeacher(this));
+    }
+
+    public List<Klass> getClasses() {
+        return klasses;
+    }
+
+    @Override
+    public String introduce() {
+        return klasses == null || klasses.isEmpty()?
+                super.introduce() + " I am a Teacher. I teach No Class."
+                :
+                super.introduce() +
+                        String.format(" I am a Teacher. I teach Class %s.",
+                                klasses.stream()
+                                        .map(Klass::getNumber)
+                                        .map(String::valueOf)
+                                        .collect(Collectors.joining(", "))
+                        );
+    }
+
+    public String introduceWith(Student student) {
+        if (klasses.isEmpty() || !isTeaching(student))
+            return super.introduce() + String.format(" I am a Teacher. I don't teach %s.", student.getName());
+        else
+            return super.introduce() + String.format(" I am a Teacher. I teach %s.", student.getName());
+    }
+
+    public boolean isTeaching(Student student) {
+        return !klasses.stream()
+                .filter(klass ->
+                        klass.isIn(student))
+                .collect(Collectors.toList())
+                .isEmpty();
+    }
+
+    public void handleNewStudentJoiningClass(Klass klass, Student student) {
+        System.out.print(String.format("I am %s. I know %s has joined %s.\n", this.getName(), student.getName(), klass.getDisplayName()));
+    }
+    
+    public void handleClassLeaderChange(Klass klass, Student student) {
+        System.out.print(String.format("I am %s. I know %s become Leader of %s.\n", this.getName(), student.getName(), klass.getDisplayName()));
+    }
 }
