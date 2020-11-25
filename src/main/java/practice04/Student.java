ace84db8138691d8cf0778819b68c662c9940dd2
Calvin Chu <calvin.chu@oocl.com>
2020-11-25 14:49:24 +0800

    [Calvin] feat: pracice 4

--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,23 @@
 package practice04;
 
-public class Student {
+public class Student extends Person{
+    private Integer klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Integer klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + String.format(" I am a Student. I am at Class %s.", klass);
+    }
 }
