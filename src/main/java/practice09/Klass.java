ef692c409ce3e664b3a0fbbd83c7b8e3829819b0
Calvin Chu <calvin.chu@oocl.com>
2020-11-25 16:44:59 +0800

    [Calvin] feat: pracice 9

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,43 @@
 package practice09;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    private Integer number;
+    private Student leader;
+    private List<Student> students = new ArrayList<>();
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void assignLeader(Student leader) {
+        if (!students.contains(leader)) {
+            System.out.print("It is not one of us.\n");
+        }
+        else {
+            this.leader = leader;
+        }
+    }
+
+    public String getDisplayName() {
+        return String.format("Class %d", number);
+    }
+
+    public void appendMember(Student student) {
+        students.add(student);
+    }
 }
--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,55 @@
 package practice09;
 
-public class Teacher {
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Teacher extends Person {
+    private List<Klass> klasses;
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, List<Klass> klasses) {
+        super(id, name, age);
+        this.klasses = klasses;
+    }
+
+    public List<Klass> getClasses() {
+        return klasses;
+    }
+
+    public void setClasses(List<Klass> klasses) {
+        this.klasses = klasses;
+    }
+
+    @Override
+    public String introduce() {
+        return klasses == null || klasses.isEmpty()?
+                super.introduce() + " I am a Teacher. I teach No Class."
+                :
+                super.introduce() +
+                        String.format(" I am a Teacher. I teach Class %s.",
+                            klasses.stream()
+                            .map(Klass::getNumber)
+                            .map(String::valueOf)
+                            .collect(Collectors.joining(", "))
+                );
+    }
+
+    public String introduceWith(Student student) {
+        if (klasses.isEmpty() || !isTeaching(student))
+            return super.introduce() + String.format(" I am a Teacher. I don't teach %s.", student.getName());
+        else
+            return super.introduce() + String.format(" I am a Teacher. I teach %s.", student.getName());
+    }
+
+    public boolean isTeaching(Student student) {
+        return !klasses.stream()
+                .filter(klass ->
+                        klass.getNumber().equals(student.getKlass().getNumber()))
+                .collect(Collectors.toList())
+                .isEmpty();
+    }
 }
