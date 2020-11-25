3a5717b43091ec571f6a4f53c6e8c2f273d60976
Calvin Chu <calvin.chu@oocl.com>
2020-11-25 14:55:28 +0800

    [Calvin] feat: pracice 5

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,30 @@
 package practice05;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Integer klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Integer klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return klass == null?
+            super.introduce() + " I am a Teacher. I teach No Class."
+            :
+            super.introduce() + String.format(" I am a Teacher. I teach Class %d.", klass);
+    }
 }
