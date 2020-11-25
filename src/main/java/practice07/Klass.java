69fc5c50315fe9926197d10f5a1f884b560f6d96
Calvin Chu <calvin.chu@oocl.com>
2020-11-25 15:23:36 +0800

    [Calvin] feat: pracice 7

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,30 @@
 package practice07;
 
 public class Klass {
+    private Integer number;
+    private Student leader;
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void assignLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public String getDisplayName() {
+        return String.format("Class %d", number);
+    }
 }
--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,30 @@
 package practice07;
 
-public class Student {
+public class Student extends Person {
+    private Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        String introduction = super.introduce() + " I am a Student.";
+        if (klass.getLeader() != null && klass.getLeader().equals(this)) {
+            introduction += String.format(" I am Leader of %s.", klass.getDisplayName());
+        }
+        else {
+            introduction += String.format(" I am at %s.", klass.getDisplayName());
+        }
+        return introduction;
+    }
 }
